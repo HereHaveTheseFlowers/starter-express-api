744d3aba98eaf05488ed0103549de178a6c80586
herehavetheseflowers <herehavetheseflowers@gmail.com>
2022-11-16 12:33:09 +0400

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+app.use(cors({
+    origin: 'https://herehavetheseflowers.com'
+  }));
+
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 3000)
