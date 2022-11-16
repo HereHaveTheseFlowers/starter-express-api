d02102335e12e7da593c44d6ce5d8ff06e40b275
herehavetheseflowers <herehavetheseflowers@gmail.com>
2022-11-16 12:36:37 +0400

    new

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const app = express()
-app.use(cors({
-    origin: 'https://herehavetheseflowers.com'
-  }));
+app.use(cors());
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
