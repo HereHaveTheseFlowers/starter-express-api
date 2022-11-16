c36426c7bf0d87d162f12697e6432c9b9b644c70
herehavetheseflowers <herehavetheseflowers@gmail.com>
2022-11-16 12:42:33 +0400

    login and password

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ app.use(cors({
     origin: 'https://herehavetheseflowers.com'
   }));
 
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+app.use(express.json())
+
+app.post("/auth", function (req, res) {
+  const  { login, password } = req.body;
+  if(!login || !password) {
+    res.status(418).send({ message: 'Provide a login and a password' });
+  }
+  if(login !== process.env.LOGIN || password !== process.env.PASSWORD) {
+    res.status(404).send({ message: 'Wrong password' });
+  }
+  res.status(200).send({ message: 'Molodec' })
 })
+
 app.listen(process.env.PORT || 3000)
